52e2ff8119f5a45e00aa5911c9eab0b1526f5f1d
markthehuman <markthehuman@gmail.com>
2026-07-22 11:48:28 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+const toggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
 
+toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
